c606024b340ad21fcc8c264be9a2a20357f35f56
Darshan raj <37452081+darshanraj24@users.noreply.github.com>
2022-08-17 21:08:56 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// var a=10
+// if(true){
+// var a=20
+// console.log(a);  //20
+// }
+// console.log(a);  //20
+
+//.........
+
+// var, let , const
+let  a=20
+
+function app(a) {
+    
+    console.log(a); //20
+}
+app()
+console.log(a);
